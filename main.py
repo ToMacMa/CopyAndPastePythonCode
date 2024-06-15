99901762b904bdaa628f6643b6548815fe90d0a8
ToMacMa <126681860+ToMacMa@users.noreply.github.com>
2024-06-15 16:06:05 +0200

    Update main.py

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ def onButtonClick(btnId):
         btnWindow.mainloop()
     if btnId == 2:
         btnWindow = tk.Tk()
-        btnWindow.title("Math copyables")
+        btnWindow.title("Random copyables")
         btnWindow.geometry(defaultBtnWindowSettings[0])
         
         createBtnWindowContents(btnWindow,btnId)
@@ -60,7 +60,15 @@ def onButtonClick(btnId):
         createBtnWindowContents(btnWindow,btnId)
 
         btnWindow.mainloop()
+    
+    if btnId == 2-1:
+        btnWindow = tk.Tk()
+        btnWindow.title("Download a file (request package)")
+        btnWindow.geometry(defaultBtnWindowSettings[1])
 
+        createBtnWindowContents(btnWindow,btnId)
+
+        btnWindow.mainloop()
 def createBtnWindowContents(btnWindow, btnId):
     if btnId == 1:
         button = tk.Button(btnWindow, text="Window with no contents",width=20,height=5, command= lambda: onButtonClick(1-1))
@@ -122,6 +130,26 @@ def createBtnWindowContents(btnWindow, btnId):
         buttonCopy = tk.Button(btnWindow, text="Copy",width=5,height=5,command= lambda: copy2clip(contents))
         buttonCopy.place(x=0,y=90)
 
+    if btnId == 2:
+        button = tk.Button(btnWindow, text="Download a file (request package)",width=25,height=5, command= lambda: onButtonClick(2-1))
+        button.place(x=10,y=0)
+    
+    if btnId == 2-1:
+
+
+        with open("./copyFiles/reDownloadFile.txt", "r") as file:
+            contents = file.read()
+
+        text = tk.Text(btnWindow,width=600,height=600); text.insert(1.0, contents); text.place(x=45,y=0)
+
+        text.configure(state="disabled")
+        text.configure(inactiveselectbackground=text.cget("selectbackground"))
+
+        #buttonRun = tk.Button(btnWindow, text="Run", width=5,height=5,command= lambda: runCode(contents))
+        #buttonRun.place(x=0,y=0)
+
+        buttonCopy = tk.Button(btnWindow, text="Copy",width=5,height=5,command= lambda: copy2clip(contents))
+        buttonCopy.place(x=0,y=90)
         
 
 def runCode(code):
@@ -138,8 +166,8 @@ def StartWindow(title):
     button = tk.Button(root, text="Tkinter",width=10,height=5, command= lambda: onButtonClick(1))
     button.place(x=0,y=0)
 
-    #button = tk.Button(root, text="Math",width=10,height=5, command= lambda: onButtonClick(2))
-    #button.place(x=100,y=0)
+    button = tk.Button(root, text="Random Copyables",width=15,height=5, command= lambda: onButtonClick(2))
+    button.place(x=100,y=0)
 
     #button = tk.Button(root, text="Settings",width=10,height=5, command= lambda: onButtonClick("settings"))
     #button.pack()
